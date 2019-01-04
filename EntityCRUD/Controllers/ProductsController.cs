db9d0efcc6bc098345fbf95bab4083930ecd02c8
bembade <suraj1bembade@gmail.com>
2019-01-04 17:17:32 +0530

    yes

---
+++ EntityCRUD/Controllers/ProductsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityCRUD.Models;
+using System.Net;
+    
+
+namespace EntityCRUD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsConnection db = new ProductsConnection();
+
+        public ViewResult Index()
+        {
+            return View(db.ProductsTable.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id Required");
+
+            }
+            Product product = db.ProductsTable.Find(id);
+            if (product == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product Not Found");
+            }
+            return View(product);
+        }
+        public ViewResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult CreatePost()
+        {
+            if(ModelState.IsValid)
+            {
+                Product product = new Product();
+                TryUpdateModel(product);
+                db.ProductsTable.Add(product);
+                db.SaveChanges();
+            }
+              return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id Required");
+            }
+            Product product = db.ProductsTable.Find(id);
+            if(product==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product not found");
+            }
+            return View(product);
+  
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+
+        public ActionResult Edit(int id)
+        {
+            Product product = db.ProductsTable.Single(x => x.ProductId == id);
+            UpdateModel(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Id Required");
+            }
+            Product product = db.ProductsTable.Find(id);
+            if(product==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product not found");
+            }
+            return View(product);
+
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+         public ActionResult Delete(int id)
+        {
+            Product product = db.ProductsTable.Find(id);
+            db.ProductsTable.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+
+
+
+
+
+
+            
+
+
+        }
+
+
+
+
+    }
+}
---
+++ EntityCRUD/Models/ProductsConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EntityCRUD.Models
+{
+    public class ProductsConnection:DbContext
+    {
+        public DbSet<Product> ProductsTable { get; set; }
+    }
+}
